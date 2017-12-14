e682ea2851d3ff6f7d848d46065e7fc1719e1299
UBISOFT-ORG\agargasson <thebossofrpg@msn.com>
2017-12-14 11:15:00 +0100

    push

---
+++ Assets/CellularAutomate.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CellularAutomate : MonoBehaviour
+{
+    int[] cells;
+    int[] ruleSet;
+    int width = 200;
+    int currentGeneration = 0;
+    List<GameObject> allGO = new List<GameObject>();
+    List<SpriteRenderer> allSR = new List<SpriteRenderer>();
+
+    public GameObject blackCell = null;
+    public GameObject whiteCell = null;
+    public int generationNumber = 20;
+
+    private void Start()
+    {
+        cells = new int[width];
+        ruleSet = new int[8];
+
+        //normal pattern 
+        for (int i = 0; i < cells.Length; i++) { cells[i] = 0; }
+        cells[cells.Length / 2] = 1;
+
+        StartCoroutine("Proceed");
+
+        int w = 10; //cell size
+
+        for (int g = 0; g < generationNumber; g++)
+        {
+            for (int i = 0; i < cells.Length; i++)
+            {
+                GameObject spawn = null;
+                if (cells[i] == 1) spawn = Instantiate(blackCell);
+                else spawn = Instantiate(whiteCell);
+
+                spawn.transform.position = new Vector3(i * w, g * -w);
+                allGO.Add(spawn);
+                allSR.Add(spawn.GetComponent<SpriteRenderer>());
+            }
+        }
+
+    }
+
+    IEnumerator Proceed()
+    {
+        int index = 30;
+
+        while(index < 255)
+        {
+            Debug.Log("index:" + index);
+
+            for (int i = 0; i < cells.Length; i++) { cells[i] = 0; }
+            cells[cells.Length / 2] = 1;
+
+            DefineNumber(index);
+
+            Render();
+            currentGeneration++;
+
+            for (int i = 0; i < generationNumber; i++)
+            {
+                Generate();
+            }
+
+            yield return new WaitForSeconds(1.0f);
+            index++;
+            currentGeneration = 0;
+        }
+    }
+
+    private void Generate()
+    {
+        int[] newcells = new int[cells.Length];
+
+        // For left case
+        int left = cells[cells.Length - 1];
+        int middle = cells[0];
+        int right = cells[1];
+        int newState = Rules(left, middle, right);
+        newcells[0] = newState;
+
+        for (int i = 1; i < cells.Length - 1; i++)
+        {
+            left = cells[i - 1];
+            middle = cells[i];
+            right = cells[i + 1];
+            newState = Rules(left, middle, right);
+            newcells[i] = newState;
+        }
+
+        // For right case
+        left = cells[cells.Length - 2];
+        middle = cells[cells.Length - 1];
+        right = cells[0];
+        newState = Rules(left, middle, right);
+        newcells[cells.Length - 1] = newState;
+
+        cells = newcells;
+
+        Render();
+
+        currentGeneration++;
+    }
+
+    private void Render()
+    {
+        int index = currentGeneration * width;
+        if (index == width * generationNumber) return;
+        for (int i = 0; i < cells.Length; i++)
+        {
+            if (cells[i] == 1) allSR[index + i].color = Color.black;
+            else allSR[index + i].color = Color.white;
+        }
+    }
+
+    int Rules(int a, int b, int c)
+    {
+        string s = "" + a + b + c;
+
+        int index = System.Convert.ToInt32(s, 2);
+
+        return ruleSet[index];
+    }
+
+    void DefineNumber(int number)
+    {
+        int temp = number;
+        int[] bin = new int[8];
+        int index = 0;
+        while(temp > 0)
+        {
+            bin[index] = temp % 2;
+            temp /= 2;
+            index++;
+        }
+
+        int[] final = new int[8];
+
+        index = 0;
+        for (int i = 7; i != 0; i--)
+        {
+            final[index] = bin[i];
+            index++;
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            ruleSet[i] = final[i];
+        }
+    }
+
+    void ChooseRuleSet(int number)
+    {
+        if (number == 99)
+        {
+            ruleSet[0] = 0;
+            ruleSet[1] = 1;
+            ruleSet[2] = 0;
+            ruleSet[3] = 1;
+            ruleSet[4] = 1;
+            ruleSet[5] = 0;
+            ruleSet[6] = 1;
+            ruleSet[7] = 0;
+        }
+
+        else if (number == 222)
+        {
+            ruleSet[0] = 0;
+            ruleSet[1] = 1;
+            ruleSet[2] = 1;
+            ruleSet[3] = 1;
+            ruleSet[4] = 1;
+            ruleSet[5] = 0;
+            ruleSet[6] = 1;
+            ruleSet[7] = 1;
+        }
+
+        else if (number == 190)
+        {
+            ruleSet[0] = 0;
+            ruleSet[1] = 1;
+            ruleSet[2] = 1;
+            ruleSet[3] = 1;
+            ruleSet[4] = 1;
+            ruleSet[5] = 1;
+            ruleSet[6] = 0;
+            ruleSet[7] = 1;
+        }
+
+        //0001 1110
+        else if (number == 30)
+        {
+            ruleSet[0] = 0;
+            ruleSet[1] = 1;
+            ruleSet[2] = 1;
+            ruleSet[3] = 1;
+            ruleSet[4] = 1;
+            ruleSet[5] = 0;
+            ruleSet[6] = 0;
+            ruleSet[7] = 0;
+        }
+
+        //0110 1110 
+        else if (number == 110)
+        {
+            ruleSet[0] = 0;
+            ruleSet[1] = 1;
+            ruleSet[2] = 1;
+            ruleSet[3] = 1;
+            ruleSet[4] = 0;
+            ruleSet[5] = 1;
+            ruleSet[6] = 1;
+            ruleSet[7] = 0;
+        }
+    }
+}
---
+++ Assets/CellularAutomate2D.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CellularAutomate2D : MonoBehaviour
+{
+    int[,] board;
+    int rows = 100;
+    int columns = 200;
+    List<GameObject> allGO = new List<GameObject>();
+    List<SpriteRenderer> allSR = new List<SpriteRenderer>();
+
+    public GameObject blackCell = null;
+    public GameObject whiteCell = null;
+
+    private void Start()
+    {
+        board = new int[columns, rows];
+
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                board[x, y] = Random.Range(0, 2);
+            }
+        }
+
+        int w = 10; //cell size
+
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                GameObject spawn = null;
+                if (board[x, y] == 1) spawn = Instantiate(blackCell);
+                else spawn = Instantiate(whiteCell);
+
+                spawn.transform.position = new Vector3(x * w, y * -w);
+                allGO.Add(spawn);
+                allSR.Add(spawn.GetComponent<SpriteRenderer>());
+            }
+        }
+
+        //Generate();
+        StartCoroutine("Proceed");
+    }
+
+    IEnumerator Proceed()
+    {
+        int index = 0;
+        int iteration = 50;
+
+        while (index < iteration)
+        {
+            Generate();
+            yield return new WaitForSeconds(0.5f);
+            index++;
+        }
+    }
+
+    private void Generate()
+    {
+        int[,] next = new int[columns, rows];
+
+        for (int x = 1; x < columns - 1; x++)
+        {
+            for (int y = 1; y < rows - 1; y++)
+            {
+                int neighbors = 0;
+
+                for(int i = -1; i <= 1; i++)
+                {
+                    for(int j = -1; j <= 1; j++)
+                    {
+                        neighbors += board[x + i, y + j];
+                    }
+                }
+
+                neighbors -= board[x, y];
+
+                if ((board[x,y] == 1) && (neighbors < 2)) next[x,y] = 0;
+                else if ((board[x,y] == 1) && (neighbors > 3)) next[x,y] = 0;
+                else if ((board[x,y] == 0) && (neighbors == 3)) next[x,y] = 1;
+                else next[x,y] = board[x,y];
+
+            }
+        }
+
+        board = next;
+
+        Render();
+    }
+
+    private void Render()
+    {
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                if (board[x, y] == 1) allSR[x * rows + y].color = Color.black;
+                else allSR[x * rows + y].color = Color.white;
+            }
+        }
+    }
+}
